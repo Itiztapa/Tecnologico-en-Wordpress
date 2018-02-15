1628357066898a9c920ca6d9993410d8c9d9f929
Abraham <abrahamzokerolero@gmail.com>
2018-02-15 15:47:24 -0600

    Se agrego el archivo 404.php el cual contiene la estructura HTML de respuesta del servidor en caso de hipervinculos rotos.

---
+++ 404.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The template for displaying 404 pages (Not Found)
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
+
+get_header(); ?>
+
+<h2><a href="<?php bloginfo('home'); ?>" Haz clic aquí </a></h2>
+
+<center><img src="http://www.itiztapalapa.edu.mx/wp-content/themes/Tecnologico/images/404-Imagen-6.png"></center>
+
+
+<?php get_footer(); ?>
+
